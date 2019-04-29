4d2bb6594fae8a9b11db10e732608efddf896893
mikoto2000 <mikoto2000@gmail.com>
2019-04-30 05:06:38 +0900

    初期化処理を実装。
    以下の条件で初期化。
    
    - 完全同期(同期ごとにフルテキストをやり取り)
    - コード補完機能有効

--- src/main/java/jp/dip/oyasirazu/lsp4snippet/SnippetLanguageServer.java
+++ src/main/java/jp/dip/oyasirazu/lsp4snippet/SnippetLanguageServer.java
@@ -17,6 +17,15 @@ public class SnippetLanguageServer implements LanguageServer, LanguageClientAwar
      */
     public SnippetLanguageServer() {}
 
+    /**
+     * 初期化パラメーターを受け取って結果を返す
+     *
+     * 本 LSP 実装では、以下の条件で初期化を行う。
+     * <ul>
+     *   <li>完全同期(同期ごとにフルテキストをやり取り)</li>
+     *   <li>コード補完機能有効</li>
+     * </ul>
+     */
     @Override
     public CompletableFuture<InitializeResult> initialize(InitializeParams params) {
 
@@ -24,7 +33,32 @@ public class SnippetLanguageServer implements LanguageServer, LanguageClientAwar
             client.logMessage( new MessageParams(MessageType.Info, "Start initialize."));
         }
 
-        return CompletableFuture.completedFuture(null);
+        // Access `https://microsoft.github.io/language-server-protocol/specification`
+        // and search `ServerCapabilities`(contained 'completionProvider' property),
+        // and search `CompletionOptions`.
+
+        // 補完オプションの作成
+        var completionOptions = new CompletionOptions();
+        // コード補完を ON にする
+        completionOptions.setResolveProvider(true);
+
+        // Access `https://microsoft.github.io/language-server-protocol/specification`
+        // and search `ServerCapabilities`(contained 'textDocumentSync' property),
+        // and search `TextDocumentSyncKind`.
+
+        // サーバーのサポート機能を表すオブジェクトを作成
+        var capabilities = new ServerCapabilities();
+        // クライアントからサーバーに対して、毎回ドキュメント全体の送信を行う。
+        capabilities.setTextDocumentSync(TextDocumentSyncKind.Full);
+        // 先ほど作った補完オプションを capabilities に設定
+        capabilities.setCompletionProvider(completionOptions);
+
+        // Access `https://microsoft.github.io/language-server-protocol/specification`
+        // and search `InitializeResult`(contained 'capabilities' property),
+
+        // InitializeResult を CompletableFuture にラップして返却
+        var result = new InitializeResult(capabilities);
+        return CompletableFuture.completedFuture(result);
     }
 
     @Override
