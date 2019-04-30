48de88826361507fcea46e66f190cba118316abd
mikoto2000 <mikoto2000@gmail.com>
2019-04-30 10:14:28 +0900

    カーソル位置のインデックスと文字を表示する補完アイテムを実装。

--- src/main/java/jp/dip/oyasirazu/lsp4snippet/SnippetTextDocumentService.java
+++ src/main/java/jp/dip/oyasirazu/lsp4snippet/SnippetTextDocumentService.java
@@ -1,7 +1,9 @@
 package jp.dip.oyasirazu.lsp4snippet;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import org.eclipse.lsp4j.CompletionItem;
@@ -11,6 +13,7 @@ import org.eclipse.lsp4j.DidChangeTextDocumentParams;
 import org.eclipse.lsp4j.DidCloseTextDocumentParams;
 import org.eclipse.lsp4j.DidOpenTextDocumentParams;
 import org.eclipse.lsp4j.DidSaveTextDocumentParams;
+import org.eclipse.lsp4j.Position;
 import org.eclipse.lsp4j.Range;
 import org.eclipse.lsp4j.TextEdit;
 import org.eclipse.lsp4j.services.TextDocumentService;
@@ -21,10 +24,20 @@ import org.eclipse.lsp4j.jsonrpc.messages.Either;
  * SnippetTextDocumentService
  */
 public class SnippetTextDocumentService implements TextDocumentService {
+
+    /**
+     * このサービスが管理するテキストドキュメント
+     *
+     * Key: uri, Value: textContent
+     */
+    private Map<String, StringBuilder> textDocuments;
+
     /**
      * Constructor
      */
-    public SnippetTextDocumentService() {}
+    public SnippetTextDocumentService() {
+        this.textDocuments = new HashMap<String, StringBuilder>();
+    }
 
     /**
      * ファイル・カーソル情報を受け取り、補完候補を返却する。
@@ -42,10 +55,19 @@ public class SnippetTextDocumentService implements TextDocumentService {
         var textEditItem = new CompletionItem("textEdit");
         textEditItem.setTextEdit(textEdit);
 
+        // 以下のパラメーターを設定した補完アイテム
+        // Label : 補完リクエストを出したときのカーソルインデックス
+        // Detail: 補完リクエストを出したときのカーソル位置の文字
+        var targetText = this.textDocuments.get(params.getTextDocument().getUri());
+        var cursorPosition = params.getPosition();
+        var cursorIndex = this.getIndex(targetText, cursorPosition);
+        var cursorIndexItem = new CompletionItem(String.valueOf(cursorIndex));
+        cursorIndexItem.setDetail(String.valueOf(targetText.charAt(cursorIndex)));
 
         List<CompletionItem> completionItemList = new ArrayList<>();
         completionItemList.add(labelOnlyItem);
         completionItemList.add(textEditItem);
+        completionItemList.add(cursorIndexItem);
 
 
         return CompletableFuture.completedFuture(Either.forLeft(completionItemList));
@@ -53,10 +75,26 @@ public class SnippetTextDocumentService implements TextDocumentService {
 
     @Override
     public void didOpen(DidOpenTextDocumentParams params) {
+        var uri = params.getTextDocument().getUri();
+        var textContent = new StringBuilder(params.getTextDocument().getText());
+
+        this.textDocuments.put(uri, textContent);
     }
 
+    /**
+     * ファイル更新通知を受け付ける。
+     *
+     * 本実装においては、 `TextDocumentSyncKind.Full` が指定されているものとして実装する。
+     */
     @Override
     public void didChange(DidChangeTextDocumentParams params) {
+        var uri = params.getTextDocument().getUri();
+
+        // `TextDocumentSyncKind.Full` を指定している前提で、
+        // ContentChanges の先頭テキストにファイル全文が入っているものとして処理する。
+        var textContent = new StringBuilder(params.getContentChanges().get(0).getText());
+
+        this.textDocuments.put(uri, textContent);
     }
 
     @Override
@@ -65,5 +103,25 @@ public class SnippetTextDocumentService implements TextDocumentService {
 
     @Override
     public void didSave(DidSaveTextDocumentParams params) {
+    }
+
+    private int getIndex(StringBuilder text, Position position) {
+        var lineIndex = position.getLine();
+        var characterIndex = position.getCharacter();
+
+        var firstOfLineIndex = 0;
+        for (int i = 0; i < lineIndex; i++) {
+            // TODO: `\r\n`, `\r` 改行コードへの対応
+            firstOfLineIndex = text.indexOf("\n", firstOfLineIndex + 1);
+        }
+
+        var positionIndex = firstOfLineIndex + characterIndex;
+
+        // indexOf で取得するのは `\n` のインデックスなので、 2 行目以降は +1 する。
+        if (firstOfLineIndex > 0) {
+            positionIndex++;
+        }
+
+        return positionIndex;
     }
 }
